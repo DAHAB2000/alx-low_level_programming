14de506abd65cc49ea564d0ee41cb31173dd4196
DAHAB2000 <dahabrostom3@gmail.com>
2023-06-07 05:31:38 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
- size_t i = 0;
-
- while (h)
- {
- printf("%d\n", h->n);
- h = h->next;
- i++;
- }
- return (i);
+size_t i = 0;
+while (h)
+{
+printf("%d\n", h->n);
+h = h->next;
+i++;
+}
+return (i);
 }
